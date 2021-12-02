d188ce45aec4d41ad00fb68a2ab9aabab6ab51ba
immangalang <87266699+immangalang@users.noreply.github.com>
2021-12-02 23:54:19 +0800

    complete cart page. front end complete

---
+++ frontend/src/pages/Cart.jsx
@@ -0,0 +1,243 @@
+import React from 'react';
+import styled from 'styled-components';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import { Add, Remove } from '@material-ui/icons';
+
+const Container = styled.div``;
+
+const Wrapper = styled.div`
+  padding: 20px;
+`;
+
+const Title = styled.h1`
+  font-weight: 300;
+  text-align: center;
+`;
+
+const Top = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 20px;
+`;
+
+const TopButton = styled.button`
+  padding: 10px;
+  cursor: pointer;
+  border: ${(props) => props.type === 'filled' && 'none'};
+  background-color: ${(props) =>
+    props.type === 'filled' ? 'black' : 'transparent'};
+  color: ${(props) => props.type === 'filled' && 'white'};
+`;
+
+const TopTexts = styled.div`
+  display: flex;
+`;
+
+const TopText = styled.p`
+  text-decoration: underline;
+  cursor: pointer;
+  margin: 0 10px;
+`;
+
+const Bottom = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+const Info = styled.div`
+  flex: 3;
+`;
+
+const Product = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const ProductDetails = styled.div`
+  flex: 2;
+  display: flex;
+`;
+
+const Image = styled.img`
+  width: 150px;
+`;
+
+const Details = styled.div`
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`;
+
+const ProductName = styled.p``;
+
+const ProductId = styled.span``;
+
+const ProductColor = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${(props) => props.color};
+`;
+
+const ProductSize = styled.span``;
+
+const PriceDetail = styled.span`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+const ProductAmountContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const ProductAmount = styled.div`
+  font-size: 1.15rem;
+  margin: 5px;
+`;
+
+const ProductPrice = styled.div`
+  font-style: 30px;
+  margin: 5px;
+`;
+
+const Hr = styled.hr`
+  background-color: #ebebeb;
+  border: none;
+  height: 2px;
+  margin: 5px 0;
+`;
+
+const Summary = styled.div`
+  flex: 1;
+  border: 0.5px solid lightgray;
+  border-radius: 10px;
+  padding: 20px;
+  height: 50vh;
+`;
+
+const SummaryTitle = styled.h1`
+  font-weight: 200;
+`;
+
+const SummaryItem = styled.div`
+  margin: 30px 0;
+  display: flex;
+  justify-content: space-between;
+  font-weight: ${(props) => props.type === 'total' && '500'};
+  font-size: ${(props) => props.type === 'total' && '1.1rem'};
+`;
+
+const SummaryItemText = styled.span``;
+
+const SummaryItemPrice = styled.span``;
+
+const Button = styled.button`
+  width: 100%;
+  padding: 10px;
+  background-color: black;
+  color: white;
+  font-weight: 600;
+`;
+
+const Cart = () => {
+  return (
+    <Container>
+      <Navbar />
+      <Wrapper>
+        <Title>Your Cart</Title>
+        <Top>
+          <TopButton>Continue Shopping</TopButton>
+          <TopTexts>
+            <TopText>Shopping Bag(2)</TopText>
+            <TopText>Your Wishlist</TopText>
+          </TopTexts>
+          <TopButton type="filled">Checkout</TopButton>
+        </Top>
+        <Bottom>
+          <Info>
+            <Product>
+              <ProductDetails>
+                <Image src="https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/437242/item/goods_69_437242.jpg?width=1600&impolicy=quality_75" />
+                <Details>
+                  <ProductName>
+                    <b>Product:</b> GRAY SHIRT
+                  </ProductName>
+                  <ProductId>
+                    <b>ID:</b> 253434
+                  </ProductId>
+                  <ProductColor color="black" />
+                  <ProductSize>
+                    <b>Size:</b> M
+                  </ProductSize>
+                </Details>
+              </ProductDetails>
+              <PriceDetail>
+                <ProductAmountContainer>
+                  <Add />
+                  <ProductAmount>1</ProductAmount>
+                  <Remove />
+                </ProductAmountContainer>
+                <ProductPrice>Php 290</ProductPrice>
+              </PriceDetail>
+            </Product>
+            <Hr />
+            <Product>
+              <ProductDetails>
+                <Image src="https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/437242/item/goods_69_437242.jpg?width=1600&impolicy=quality_75" />
+                <Details>
+                  <ProductName>
+                    <b>Product:</b> GRAY SHIRT
+                  </ProductName>
+                  <ProductId>
+                    <b>ID:</b> 253434
+                  </ProductId>
+                  <ProductColor color="black" />
+                  <ProductSize>
+                    <b>Size:</b> M
+                  </ProductSize>
+                </Details>
+              </ProductDetails>
+              <PriceDetail>
+                <ProductAmountContainer>
+                  <Add />
+                  <ProductAmount>1</ProductAmount>
+                  <Remove />
+                </ProductAmountContainer>
+                <ProductPrice>Php 290</ProductPrice>
+              </PriceDetail>
+            </Product>
+          </Info>
+          <Summary>
+            <SummaryTitle>Summary</SummaryTitle>
+            <SummaryItem>
+              <SummaryItemText>Subtotal</SummaryItemText>
+              <SummaryItemPrice>Php 290</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem>
+              <SummaryItemText>Estimated Shipping</SummaryItemText>
+              <SummaryItemPrice>Php 45</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem>
+              <SummaryItemText>Shipping Discount</SummaryItemText>
+              <SummaryItemPrice>Php 40</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem type="total">
+              <SummaryItemText>Total</SummaryItemText>
+              <SummaryItemPrice>Php 295</SummaryItemPrice>
+            </SummaryItem>
+            <Button>Checkout Now</Button>
+          </Summary>
+        </Bottom>
+      </Wrapper>
+      <Footer />
+    </Container>
+  );
+};
+
+export default Cart;
